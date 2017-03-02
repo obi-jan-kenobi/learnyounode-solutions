22ed58c0816ecc12bfb5e00328e3d60c1a9619b2
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-02 09:51:31 +0100

    moved learnyounode solutions

--- 02-baby-steps.js
+++
@@ -1,7 +0,0 @@
-const [x, y, ...summands] = process.argv
-
-function sum(summands) {
-  return summands.reduce((prev, curr) => +prev + +curr)
-}
-
-console.log(sum(summands))
--- 07-http-client.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-
-http.get(process.argv[2], res => {
-  res.setEncoding('utf8')
-  res.on('error', console.error)
-  res.on('data', console.log)
-}).on('error', console.error)
